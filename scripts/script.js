78be364e154aff6391a46c2856516039df00ac0b
Michail <tillin59rus@yandex.ru>
2019-10-13 23:29:57 +0500

    Добавляется новая пустая запись

--- scripts/script.js
+++ scripts/script.js
@@ -1,53 +1,74 @@
 var counter = 0;
 
-function addInvoice() {
-    var table = document.getElementById("invoicesTable");
-
-    var invoice = document.createElement('tr');
-    invoice.className = 'invoice';
-    invoice.id = 'invoice-' + counter;
-    for(let i = 0; i < 5; i += 1) {
-        let column = document.createElement('td');
-        switch(i) {
-            case 0:
-                column.innerHTML = 'дата создания';
-                break;
-            case 1:
-                column.innerHTML = 'номер';
-                break;
-            case 2:
-                column.innerHTML = 'дата поставки';
-                break;
-            case 3:
-                column.innerHTML = 'комментарий';
-                break;
-            case 4:
-                column.className = "buttonColumn";
-
-                var editButton = document.createElement('button');
-                editButton.innerHTML = 'Edit';
-                editButton.onclick = function() {
-                    location.href="edit-form.html"
-                }
-                column.appendChild(editButton);
-                
-                var deleteButton = document.createElement('button');
-                deleteButton.innerHTML = 'Delete';
-                deleteButton.id = 'deleteButton-' + counter;
-                
-                deleteButton.onclick = function() {
-                    invoice.parentNode.removeChild(invoice);
-                };
-                column.appendChild(deleteButton);
-                
-                //column.innerHTML = '<button>Edit</button> <button>Delete</button>';
-                break;
-            default:
-                break;
-        }
-        invoice.appendChild(column);
+let addInvoice = async () =>  {
+    newInvoice = {
+        comment: null,
+        date_created: null,
+        date_due: null,
+        date_supply: null,
+        direction: null,//Сделай guid
     }
-    table.appendChild(invoice);
+    let response = await fetch('http://localhost:3000/invoices', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(newInvoice),
+    });
+
+    if(response.ok) {
+        getInvoices();
+    }
+    else {
+        console.error('Error: ${response.status}');
+    }
+    // var table = document.getElementById("invoicesTable");
+
+    // var invoice = document.createElement('tr');
+    // invoice.className = 'invoice';
+    // invoice.id = 'invoice-' + counter;
+    // for(let i = 0; i < 5; i += 1) {
+    //     let column = document.createElement('td');
+    //     switch(i) {
+    //         case 0:
+    //             column.innerHTML = 'дата создания';
+    //             break;
+    //         case 1:
+    //             column.innerHTML = 'номер';
+    //             break;
+    //         case 2:
+    //             column.innerHTML = 'дата поставки';
+    //             break;
+    //         case 3:
+    //             column.innerHTML = 'комментарий';
+    //             break;
+    //         case 4:
+    //             column.className = "buttonColumn";
+
+    //             var editButton = document.createElement('button');
+    //             editButton.innerHTML = 'Edit';
+    //             editButton.onclick = function() {
+    //                 location.href="edit-form.html"
+    //             }
+    //             column.appendChild(editButton);
+                
+    //             var deleteButton = document.createElement('button');
+    //             deleteButton.innerHTML = 'Delete';
+    //             deleteButton.id = 'deleteButton-' + counter;
+                
+    //             deleteButton.onclick = function() {
+    //                 invoice.parentNode.removeChild(invoice);
+    //             };
+    //             column.appendChild(deleteButton);
+                
+    //             //column.innerHTML = '<button>Edit</button> <button>Delete</button>';
+    //             break;
+    //         default:
+    //             break;
+    //     }
+    //     invoice.appendChild(column);
+    // }
+    // table.appendChild(invoice);
 }
 
 function remove() {
@@ -58,7 +79,7 @@ function remove() {
 
 
 let getInvoices = async () => {
-    let response = await fetch('http://localhost:3000/invoices')
+    let response = await fetch('http://localhost:3000/invoices');
     if(response.ok) {
         let data = await response.json();
         console.log(data);
