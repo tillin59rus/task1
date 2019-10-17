8c3a35c04978b6d6cd825f4af974203df48d1537
Michail <tillin59rus@yandex.ru>
2019-10-18 00:29:48 +0500

    Оформлена форма редактирования

--- scripts/script.js
+++ scripts/script.js
@@ -22,53 +22,6 @@ let addInvoice = async () =>  {
     else {
         console.error('Error: ${response.status}');
     }
-    // var table = document.getElementById("invoicesTable");
-
-    // var invoice = document.createElement('tr');
-    // invoice.className = 'invoice';
-    // invoice.id = 'invoice-' + counter;
-    // for(let i = 0; i < 5; i += 1) {
-    //     let column = document.createElement('td');
-    //     switch(i) {
-    //         case 0:
-    //             column.innerHTML = 'дата создания';
-    //             break;
-    //         case 1:
-    //             column.innerHTML = 'номер';
-    //             break;
-    //         case 2:
-    //             column.innerHTML = 'дата поставки';
-    //             break;
-    //         case 3:
-    //             column.innerHTML = 'комментарий';
-    //             break;
-    //         case 4:
-    //             column.className = "buttonColumn";
-
-    //             var editButton = document.createElement('button');
-    //             editButton.innerHTML = 'Edit';
-    //             editButton.onclick = function() {
-    //                 location.href="edit-form.html"
-    //             }
-    //             column.appendChild(editButton);
-                
-    //             var deleteButton = document.createElement('button');
-    //             deleteButton.innerHTML = 'Delete';
-    //             deleteButton.id = 'deleteButton-' + counter;
-                
-    //             deleteButton.onclick = function() {
-    //                 invoice.parentNode.removeChild(invoice);
-    //             };
-    //             column.appendChild(deleteButton);
-                
-    //             //column.innerHTML = '<button>Edit</button> <button>Delete</button>';
-    //             break;
-    //         default:
-    //             break;
-    //     }
-    //     invoice.appendChild(column);
-    // }
-    // table.appendChild(invoice);
 }
 
 function remove() {
